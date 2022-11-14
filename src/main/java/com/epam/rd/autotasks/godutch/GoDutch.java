37970ac0624106c943703dce3cba9e63184ad6e0
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-14 20:40:11 +0300

    Go Dutch

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,23 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
-        //Write code here
+        Scanner scan = new Scanner(System.in);
+        int accountAll = scan.nextInt();
+        int countGuest = scan.nextInt();
+        int accountGuest = (int)(1.1*accountAll / countGuest);
+        int i;
+        if (accountAll < 0) {
+            System.out.println("Bill total amount cannot be negative");
+        } else if (countGuest <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+        } else {
+            for ( i = 0; i < accountGuest; i++) {
+            }
+            System.out.println(i);
+        }
     }
 }
